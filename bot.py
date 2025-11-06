93326bf530b76ab16bc75c5b56800861a174b8d4
wizardweblindor <wizard.web.lindor@gmail.com>
2025-11-06 23:09:03 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,8 +36,8 @@ app = ApplicationBuilder().token(BOT_TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, chat))
 
-print("Bot is running...")  
-app.run_polling()  
+print("Bot is running...")
+app.run_polling()
 
 if name == "main":
 main()
