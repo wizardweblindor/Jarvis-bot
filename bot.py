0ca45ae7be6f185aec7a5eb4dc7a33fec0c9dc48
wizardweblindor <wizard.web.lindor@gmail.com>
2025-11-09 13:20:42 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -56,5 +56,9 @@ def setup_webhook():
 # Run the Flask app
 # ------------------------
 if __name__ == "__main__":
+    import threading
+    threading.Thread(target=run_bot).start()
+
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
